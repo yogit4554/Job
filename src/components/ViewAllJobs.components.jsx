c767949bddfb76db5e2a5e16a4d35ff877cf04ba
yogit4554 <yt7407286@gmail.com>
2024-07-05 11:24:47 +0530

    Added Pages Not found

--- src/components/ViewAllJobs.components.jsx
+++ src/components/ViewAllJobs.components.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ViewAllJobs() {
   return (
     <section class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <Link
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Jobs</Link
       >
     </section>
   )
